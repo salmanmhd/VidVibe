6ed716cadf4e031e99e0f24457cc7425023b0644
Md Salman <mhdsalman010@gmail.com>
2025-05-17 10:55:08 +0530

    feat: response and error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,19 @@
 import "dotenv/config";
 import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+const PORT = process.env.PORT;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`Express serer error: ${error}`);
+    });
+
+    app.listen(PORT, () => {
+      console.log(`server running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Mongo connection failed: ${error}`);
+  });
