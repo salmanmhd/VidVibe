77edb5a4872628ab6504eb4614bdf1f07f8f1c41
Md Salman <mhdsalman010@gmail.com>
2025-05-18 09:28:45 +0530

    feat: file upload, multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,64 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localPath) => {
+  try {
+    if (!localPath) {
+      console.log("Upload failed: file path not provided");
+      return null;
+    }
+
+    const response = await cloudinary.uploader.upload(localPath, {
+      resource_type: "auto",
+    });
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localPath);
+    console.log("file upload failed: ", error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
+
+// (async function () {
+//   // Upload an image
+//   const uploadResult = await cloudinary.uploader
+//     .upload(
+//       "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+//       {
+//         public_id: "shoes",
+//       }
+//     )
+//     .catch((error) => {
+//       console.log(error);
+//     });
+
+//   console.log(uploadResult);
+
+//   // Optimize delivery by resizing and applying auto-format and auto-quality
+//   const optimizeUrl = cloudinary.url("shoes", {
+//     fetch_format: "auto",
+//     quality: "auto",
+//   });
+
+//   console.log(optimizeUrl);
+
+//   // Transform the image: auto-crop to square aspect_ratio
+//   const autoCropUrl = cloudinary.url("shoes", {
+//     crop: "auto",
+//     gravity: "auto",
+//     width: 500,
+//     height: 500,
+//   });
+
+//   console.log(autoCropUrl);
+// })();
