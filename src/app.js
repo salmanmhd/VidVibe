d4f4f799cc1f242c84de228a6e742628b1db3214
Md Salman <mhdsalman010@gmail.com>
2025-05-18 11:15:02 +0530

    feat: route setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookies-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -10,22 +10,22 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(
   express.json({
     limit: "16kb",
   })
 );
-
 app.use(
   express.urlencoded({
     extended: true,
     limit: "16kb",
   })
 );
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/user", userRouter);
 
 export { app };
