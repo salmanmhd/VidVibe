1aba721ab8a9619f5d753712495a98ea054fed2a
salmanmhd <mhdsalman010@gmail.com>
2025-06-03 09:49:56 +0530

    feat: channel toggle subscription

--- src/app.js
+++ src/app.js
@@ -40,9 +40,9 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/tweet", tweetRouter);
 app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/s", subscriptionRouter);
 // app.use("/api/v1/playlist", playlistRouter);
 // app.use("/api/v1/like", likeRouter);
-// app.use("/api/v1/subscription", subscriptionRouter);
 // app.use("/api/v1/dashboard", dashboardRouter);
 // app.use("/api/v1/healthcheck", healthcheckRouter);
 
