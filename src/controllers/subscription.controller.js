4b3539c20e23b7dabe048d1d2f84b22d1b12124a
salmanmhd <mhdsalman010@gmail.com>
2025-06-03 23:27:24 +0530

    feat: complete subscription

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -15,6 +15,14 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     );
   }
 
+  if (channelId === req.user?._id) {
+    return res.status(400).json(
+      new ApiResponse(400, null, {
+        error: "You cannot subscribe to yourself",
+      })
+    );
+  }
+
   try {
     const isSubscribed = await Subscription.findOne({
       subscriber: req.user?._id,
@@ -47,11 +55,160 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+  console.log("🟡inside get subscribers");
+
+  if (!channelId || !isValidObjectId(channelId)) {
+    return res.status(400).json(
+      new ApiResponse(400, null, {
+        error: "channelId is missing or malformed",
+      })
+    );
+  }
+
+  try {
+    const aggregateQuery = [
+      {
+        $group: {
+          _id: "$channel",
+          userIds: { $addToSet: "$subscriber" },
+        },
+      },
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(channelId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userIds",
+          foreignField: "_id",
+          as: "subscribers",
+        },
+      },
+      {
+        $project: {
+          subscribers: {
+            $map: {
+              input: "$subscribers",
+              as: "user",
+              in: {
+                _id: "$$user._id",
+                username: "$$user.username",
+                email: "$$user.email",
+                fullName: "$$user.fullName",
+                avatar: "$$user.avatar",
+              },
+            },
+          },
+        },
+      },
+    ];
+
+    const subscribers = await Subscription.aggregate(aggregateQuery);
+    if (!subscribers) {
+      return res.status(500).json(
+        new ApiResponse(500, "unable to get subscribers", {
+          error: "unable to get subscribers",
+        })
+      );
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, "subscribers fetched successfully", subscribers)
+      );
+  } catch (error) {
+    res.status(500).json(
+      new ApiResponse(500, "unable to get subscribers", {
+        error: error.message,
+      })
+    );
+  }
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+
+  if (!subscriberId) {
+    return res.status(400).json(
+      new ApiResponse(400, null, {
+        error: "subscriberId is missing or malformed",
+      })
+    );
+  }
+
+  try {
+    const aggregateQuery = [
+      {
+        $group: {
+          _id: "$subscriber",
+          channels: {
+            $addToSet: "$channel",
+          },
+        },
+      },
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(subscriberId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "channels",
+          foreignField: "_id",
+          as: "subscribedTo",
+        },
+      },
+      {
+        $project: {
+          subscribedTo: {
+            $map: {
+              input: "$subscribedTo",
+              as: "user",
+              in: {
+                _id: "$$user._id",
+                channelName: "$$user.username",
+                email: "$$user.email",
+                fullName: "$$user.fullName",
+                avatar: "$$user.avatar",
+                coverImage: "$$user.coverImage",
+              },
+            },
+          },
+        },
+      },
+    ];
+
+    const subscribedChannels = await Subscription.aggregate(aggregateQuery);
+
+    if (!subscribedChannels) {
+      return res.status(500).json(
+        new ApiResponse(500, "unable to get subscribed channels", {
+          error: "unable to get subscribed channels",
+        })
+      );
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          "subscribed channels fetched successfully",
+          subscribedChannels
+        )
+      );
+  } catch (error) {
+    res.status(500).json(
+      new ApiResponse(500, "unable to get subscribed channels", {
+        error: error.message,
+      })
+    );
+  }
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
